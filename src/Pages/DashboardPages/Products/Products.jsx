8f70d303d424d266f2dc2b2587926b5be391d52b
sakil-sagor <sakilsagor76543@gmail.com>
2024-02-12 05:04:14 +0600

    make the product added function

---
+++ src/Pages/DashboardPages/Products/Products.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+const Products = () => {
+  const [product, setProduct] = useState({
+    productName: "",
+    category: "",
+    brand: "",
+    origin: "",
+    quantity: "",
+    buyPrice: "",
+    costPrice: "",
+    salePrice: "",
+    batch: "",
+    image: "",
+    showroom: "",
+  });
+
+  const handleChange = (e) => {
+    const inputName = e.target.name;
+    const value = e.target.value;
+    setProduct({ ...product, [inputName]: value });
+  };
+  const handleDetailsSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(product);
+  };
+
+  return (
+    <div>
+      <div>
+        <div>
+          <form onSubmit={handleDetailsSubmit}>
+            <div>
+              <input
+                onChange={handleChange}
+                type="text"
+                name="name"
+                id="name"
+                className="border "
+              />
+            </div>
+            <div className="my-4">
+              <input
+                onChange={handleChange}
+                type="text"
+                name="email"
+                id="email"
+                className="border "
+              />
+            </div>
+            <div>
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
